6da4d8313d228c6e7d40d9caa80317ca26fa927b
kudr1kk22 <kudr1kk22@gmail.com>
2023-05-10 12:46:21 +0300

    Initial Commit

---
+++ MobintTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobintTestApp
+//
+//  Created by Eugene Kudritsky on 10.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
